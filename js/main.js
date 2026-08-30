4383126ec4fe23628cb0bbbac2aad1271e7f9a69
MegGarcia <89491715+MegGarcia@users.noreply.github.com>
2026-08-30 19:07:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
   // Submissions/logins go through a Google Apps Script Web App bound to a
   // Google Sheet. See google-apps-script/README.md for how to deploy it
   // and get this URL.
-  var FORM_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyvorsLrGa4UJ2-93LDJByizdq8MJ5Uc1i7unDinzjZ9BxevgBvwtvvFycO_VDH6yUxrA/exec';
+  var FORM_ENDPOINT = 'https://script.google.com/macros/s/AKfycbykXicuZau62KHgfccEqbTAd5o2z8IEIKjvkzpVXr5vRkePNalIUnZ0UPtsChL2O5y-lg/exec';
 
   // ---------- Login gate ----------
   var AUTH_STORAGE_KEY = 'weddingSiteAuthed';
